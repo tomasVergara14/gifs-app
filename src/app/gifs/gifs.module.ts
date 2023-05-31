52aac2ca8abc79e79f91e763fc790b8d9f3a2acb
Tom <tomasvergara272@gmail.com>
2023-05-31 02:15:00 -0300

    Card list

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CardListComponent
   ]
 })
 export class GifsModule { }
