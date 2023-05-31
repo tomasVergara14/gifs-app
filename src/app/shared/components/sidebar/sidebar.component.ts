b9b9e1f3b19fe3c0ff40a420cf4890df840ca96c
Tom <tomasvergara272@gmail.com>
2023-05-31 01:49:52 -0300

    Sidebar

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+}
