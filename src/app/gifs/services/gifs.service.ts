192cca169817226e4a1bc8b9844bcf2b433e19c5
Tom <tomasvergara272@gmail.com>
2023-06-02 21:52:02 -0300

    Validations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,9 +13,23 @@ export class GifsService {
     return this._tagsHistory;
   }
 
+  private organizeTags( tag: string ){
+      tag = tag.toLowerCase();            // JS is case sensitive
+
+      if(this._tagsHistory.includes(tag)){
+        this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag != tag) //returns the one that are diferent from the one being search
+      }
+      this._tagsHistory.unshift(tag);
+
+      this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
   public searchTags( tag: string):void{
 
-      this._tagsHistory.unshift(tag);
+    if(tag.length === 0 ) return;  // Validate if there is a search
+    
+    this.organizeTags(tag);
+    console.log(this._tagsHistory)
   }
 
 }
