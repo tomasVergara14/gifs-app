4101cb645fec294f5ab015ec049552972f8f3523
Tom <tomasvergara272@gmail.com>
2023-06-12 23:57:35 -0300

    loader

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.scss']
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input() 
+  public url!: string;
+
+  @Input()
+  public alt!: string;
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('Method not implemented.');
+  }
+
+  onLoad(){
+    this.hasLoaded = true;
+  }
 
 }
