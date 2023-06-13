0ac91bb8eab755ed4cce45f98c20af5a5629e0fd
Tom <tomasvergara272@gmail.com>
2023-06-12 23:28:20 -0300

    lazy image

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { CardItemComponent } from './components/card-item/card-item.component';
 
 
 
 @NgModule({
   declarations: [
+    CardItemComponent,
+    CardListComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent,
-    CardItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports:[
+    CardListComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
   ]
 })
 export class GifsModule { }
